02f9bd2482880597c9681d6dce22d996ff93f06c
Muhammad Ali Danish <mdanish@lab1r4s5.42abudhabi.ae>
2024-11-28 16:25:56 +0400

    feat: finished ex05 of C01

---
+++ C01/ex05/sources/Harl.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdanish <mdanish@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:00:26 by mdanish           #+#    #+#             */
+/*   Updated: 2024/11/28 16:05:32 by mdanish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Harl.hpp"
+
+Harl::Harl(void) {
+	return;
+}
+
+Harl::~Harl(void) {
+	return;
+}
+
+void Harl::debug(void) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void Harl::info(void) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void Harl::warning(void) {
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n";
+}
+
+void Harl::error(void) {
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level) {
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*level_functions[4])();
+	level_functions[0] = &Harl::debug;
+	level_functions[1] = &Harl::info;
+	level_functions[2] = &Harl::warning;
+	level_functions[3] = &Harl::error;
+
+	for (int i = 0; i < 4; i++) {
+		if (levels[i] == level)
+			return (this->*level_functions[i])();
+	}
+	
+}
+
---
+++ C01/ex05/sources/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdanish <mdanish@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:59:15 by mdanish           #+#    #+#             */
+/*   Updated: 2024/11/28 16:02:38 by mdanish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Harl.hpp"
+
+int main() {
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("afvdafe");
+	return 0;
+}
