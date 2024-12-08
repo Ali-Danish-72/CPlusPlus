6f1b459f2b410af36b543fd99e845b4d4cfd78eb
Muhammad Ali Danish <mdanish@lab1r4s5.42abudhabi.ae>
2024-12-08 19:07:46 +0400

    Completed ex02 of C02

---
+++ C02/ex02/includes/Fixed.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdanish <mdanish@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 17:16:01 by mdanish           #+#    #+#             */
+/*   Updated: 2024/12/08 19:05:18 by mdanish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int _value;
+		static const int _fractionalBits = 8;
+
+	public:
+		Fixed(void);
+		Fixed(int const wholeNumber);
+		Fixed(float const decimalNumber);
+		Fixed(const Fixed & other);
+		~Fixed(void);
+
+		Fixed & operator = (const Fixed & other);
+		bool operator > (const Fixed & other) const;
+		bool operator < (const Fixed & other) const;
+		bool operator >= (const Fixed & other) const;
+		bool operator <= (const Fixed & other) const;
+		bool operator == (const Fixed & other) const;
+		bool operator != (const Fixed & other) const;
+		Fixed operator + (const Fixed & other) const;
+		Fixed operator - (const Fixed & other) const;
+		Fixed operator * (const Fixed & other) const;
+		Fixed operator / (const Fixed & other) const;
+		Fixed operator ++ (int);
+		Fixed operator -- (int);
+		Fixed & operator ++ (void);
+		Fixed & operator -- (void);
+
+		float toFloat(void) const;
+		int toInt(void) const;
+		int getRawBits(void) const;
+		void setRawBits(int const rawBits);
+
+		static Fixed & min(Fixed & leftNumber, Fixed & rightNumber);
+		static const Fixed & min(Fixed const &leftNumber, Fixed const &rightNumber);
+		static Fixed & max(Fixed & leftNumber, Fixed & rightNumber);
+		static const Fixed & max(Fixed const & leftNumber, Fixed const & rightNumber);
+};
+
+std::ostream & operator << (std::ostream & out, const Fixed & other);
---
+++ C02/ex02/sources/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdanish <mdanish@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/06 17:43:08 by mdanish           #+#    #+#             */
+/*   Updated: 2024/12/08 17:21:12 by mdanish          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::min( a, b ) << std::endl;
+
+	return 0;
+}
